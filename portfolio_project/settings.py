ae5467846fe4f164c9c920b25278337e1ca00f32
Akila <akila271819@gmail.com>
2025-10-22 22:21:22 +0530

    initial commit

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -14,8 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -----------------------------
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='your-secret-key')
 DEBUG = config('DJANGO_DEBUG', cast=bool, default=True)
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')], default=[])
-
+ALLOWED_HOSTS = ['*']
 # -----------------------------
 # Installed Apps
 # -----------------------------
